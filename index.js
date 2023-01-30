67dd37599d01ae57ea6e4fedac10774c507ce079
kubamika16 <kuba.mika16@gmail.com>
2023-01-30 15:08:24 +0000

    commit

--- index.js
+++ index.js
@@ -1,49 +1,71 @@
+// MON: 1-2
+// TUE: 4-5
+// WED: 8-9
+// THU: 12-13
+// FRI: 16-17
+// SAT: 20-21
+// SUN: 24-25
+
+////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////
+// Packages
 const XLSX = require('xlsx')
 
+////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////
+// Functions and variables
+const daysArray = [
+  'monday',
+  'tuesday',
+  'wednesday',
+  'thursday',
+  'friday',
+  'saturday',
+  'sunday',
+]
+
+// GETTING TO A XLLS FILE:
 // read the xlsx file
 const workbook = XLSX.readFile('./rota.xlsx')
-
 // get the first sheet
 const sheetName = workbook.SheetNames[0]
 const sheet = workbook.Sheets[sheetName]
-
 // convert the sheet to a json object
 const json = XLSX.utils.sheet_to_json(sheet)
-// console.log(json)
+console.log(json)
 
-//Wyciągnięcie z 'json' danych które zaczynają się od 'Jakub Mika'
+// Taking data from object about certain person
 const JSONobj = json.find(
   (arg) =>
-    arg['Weekly Activity Schedule for Reading Broad St'] === 'Jakub Mika',
+    arg['Weekly Activity Schedule for Reading Broad St'] === 'Alina Fartade',
 )
 console.log(JSONobj)
 
+////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////
+// MANIPULATING FIRST OBJECT
+
+// Returning a string that is saved in every empty key value
 const value = JSONobj['__EMPTY']
-
-// First, check if there is exact number. If is, then return result
-
 const valueNumber = (number) => JSONobj[`__EMPTY_${number}`]
+console.log(valueNumber(3))
 
-console.log(valueNumber(7))
-console.log(valueNumber(7) ? valueNumber(5) : 'Off')
-
-// person.daysOfTheWeek.monday.type[value] = true
-// console.log(person)
-
-// When someone has a day off, there is no such thing as a valueNumber. If that's the case, the whole message should be 'day off', not 'undefined: day off'
-const test = {
-  type: {
-    [valueNumber(11)]: valueNumber(12)
-      ? `${valueNumber(12)} - ${valueNumber(13)}`
-      : 'Day off',
-  },
+// This code is using a for-in loop to iterate through the keys of the object JSONobj. For each key, it checks if the value associated with that key is equal to the string "Core Hou". If it is, it assigns the value to a variable called value. Finally, it logs the value to the console.
+let workType = ''
+for (const key in JSONobj) {
+  if (JSONobj[key] === 'Core Hou') {
+    workType = JSONobj[key]
+  }
 }
+console.log(workType)
 
-if (test.type.key === undefined) {
-  test.type = 'Day off'
-}
-console.log(test)
-
+////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////
+// WORK ON ending object
 // Object that contains name of the person and days of the week person is working
 const person = {
   name: JSONobj['Weekly Activity Schedule for Reading Broad St'],
@@ -55,83 +77,53 @@ const person = {
     },
     tuesday: {
       type: {
-        [valueNumber(3)]: valueNumber(4)
-          ? `${valueNumber(4)} - ${valueNumber(5)}`
-          : 'Day off',
+        [workType]: `${valueNumber(4)} - ${valueNumber(5)}`,
       },
     },
     wednesday: {
       type: {
-        [valueNumber(7)]: valueNumber(8)
-          ? `${valueNumber(8)} - ${valueNumber(9)}`
-          : 'Day off',
+        [valueNumber(7)]: `${valueNumber(8)} - ${valueNumber(9)}`,
       },
     },
     thursday: {
       type: {
-        [valueNumber(11)]: valueNumber(12)
-          ? `${valueNumber(12)} - ${valueNumber(13)}`
-          : 'Day off',
+        [valueNumber(11)]: `${valueNumber(12)} - ${valueNumber(13)}`,
       },
     },
     friday: {
       type: {
-        [valueNumber(15)]: valueNumber(16)
-          ? `${valueNumber(16)} - ${valueNumber(17)}`
-          : 'Day off',
+        [valueNumber(15)]: `${valueNumber(16)} - ${valueNumber(17)}`,
       },
     },
     saturday: {
       type: {
-        [valueNumber(19)]: valueNumber(20)
-          ? `${valueNumber(20)} - ${valueNumber(21)}`
-          : 'Day off',
+        [valueNumber(19)]: `${valueNumber(20)} - ${valueNumber(21)}`,
       },
     },
     sunday: {
       type: {
-        [valueNumber(23)]: valueNumber(24)
-          ? `${valueNumber(24)} - ${valueNumber(25)}`
-          : 'Day off',
+        [valueNumber(23)]: `${valueNumber(24)} - ${valueNumber(25)}`,
       },
     },
   },
 }
 
-// Iteration of every day of the week. If numbers 3, 7, 11, 15, 19, 23 are equal to 'undefined', we have to iterate and change the object
-// Tworzę tablicę z dniami tygodnia
-const daysArray = [
-  'monday',
-  'tuesday',
-  'wednesday',
-  'thursday',
-  'friday',
-  'saturday',
-  'sunday',
-]
-// Przechodzę przez kazde dni tygodnia monday - sunday
-// Jeśli monday (0).type.key === undefined
-// monday(0).type = 'Day off'
-if (person.daysOfTheWeek[daysArray[2]].type.key === undefined)
-  person.daysOfTheWeek[daysArray[2]].type = 'Day off'
+for (let i = 0; i < daysArray.length; i++) {
+  if (Object.keys(person.daysOfTheWeek[daysArray[i]].type)[0] === 'undefined')
+    person.daysOfTheWeek[daysArray[i]].type = 'Day off'
+}
 
-// if (person.daysOfTheWeek.wednesday.type.key === undefined)
-//   person.daysOfTheWeek.wednesday.type = 'Day off'
-
-// MON: 1-2
-// TUE: 4-5
-// WED: 8-9
-// THU: 12-13
-// FRI: 16-17
-// SAT: 20-21
-// SUN: 24-25
-
+////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////
+// RESULTS
 console.log(person)
-
-//SUPER! Na tych danych mogę stworzyć obiekty!!!!!!
 
 //Przekonwertowałem tabelę z pdf na excel na tej stronie:
 //https://pdftables.com/upload/view/5b0gccyj19k9lkat0jhxiau71/rota.pdf
 //Pobrałem ten plik i zapisałem go jako rota.xlsx
 //Użyłem go w funkcji powyżej
-// Jestem na linijce 101
+
+// Sprawdzić które dane się nie zgadzają i spróbować coś wykombinować
+
+// Jeśli klucz, oraz wartość zawiera '\n' to znaczy
